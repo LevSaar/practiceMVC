e5a3ef2a7ef5bdc85aea2b624cade3580a0d94ea
LevSaar <shmeleef@bk.ru>
2019-01-08 16:28:01 +0600

    Dent clinic #1

---
+++ DentCarePractice/Views/MainController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mail;
+using System.Net;
+
+namespace DentCarePractice.Views
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SendData(string name, string phone)
+        {
+            // отправитель - устанавливаем адрес и отображаемое в письме имя
+            MailAddress from = new MailAddress("mvctest690@yandex.ru", "Lev");
+            // кому отправляем
+            MailAddress to = new MailAddress("mvctest690@yandex.ru");
+            // создаем объект сообщения
+            MailMessage m = new MailMessage(from, to);
+            // тема письма
+            m.Subject = "Theme";
+            // текст письма
+            m.Body = "<h2>Имя:</h2>"+name+"<br><br><h2>Телефон:</h2>"+phone;
+            // письмо представляет код html
+            m.IsBodyHtml = true;
+            // адрес smtp-сервера и порт, с которого будем отправлять письмо
+            SmtpClient smtp = new SmtpClient("smtp.yandex.ru", 587);
+            // логин и пароль
+            smtp.Credentials = new NetworkCredential("mvctest690@yandex.ru", "testmvc123");
+            smtp.EnableSsl = true;
+            smtp.Send(m);
+
+
+            return View("index");
+        }
+    }
+}
